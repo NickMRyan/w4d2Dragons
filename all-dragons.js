13ee355542bad15e012911cbaca8a748a519a8fd
nickmacbethryan <nick.macbeth.ryan@gmail.com>
2022-09-20 18:37:21 -0400

    first project complete

--- all-dragons.js
+++ all-dragons.js
@@ -1,7 +1,22 @@
 const FriendlyDragon = require('./classes/friendly-dragon')
-const EvilDragon = require('./classes/evil-dragon')
+const EvilDragon = require('./classes/evil-dragon');
+const Dragon = require('./classes/dragon');
+
+const falkor = new FriendlyDragon('Falkor',
+'white', ["save Atreyu from the swamp", "save Atreyu from the Nothing",
+"scare the local bullies into a dumpster"], 'Bastian');
+
+const smaug = new EvilDragon('Smaug', 'black', ["take over your mountain kingdom", "steal all your dwarven gold",
+"burn down your floating village"], 'Dwarf King');
+
+const allDragons = Dragon.getDragons(falkor, smaug);
 
 
+// console.log(allDragons)
+
+module.exports.allDragons = allDragons;
+
+// console.log(module.exports)
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
@@ -14,3 +29,6 @@ try {
 } catch {
   module.exports = null;
 }
+
+module.exports.allDragons = allDragons;
+console.log(module.exports)
