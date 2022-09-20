8a2d75d2f1819265deb3b18d9cabed0f0ba62457
nickmacbethryan <nick.macbeth.ryan@gmail.com>
2022-09-20 18:16:27 -0400

    early stage 4

--- all-dragons.js
+++ all-dragons.js
@@ -1,4 +1,7 @@
-// Your code here
+const FriendlyDragon = require('./classes/friendly-dragon')
+const EvilDragon = require('./classes/evil-dragon')
+
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
